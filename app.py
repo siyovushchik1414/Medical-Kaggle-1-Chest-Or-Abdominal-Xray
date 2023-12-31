ab2636a66e30cbbc940cbf5760c582276f8ef3c5
Hamidov Siyovush <93854666+siyovushchik1414@users.noreply.github.com>
2023-12-31 17:04:41 +0500

    Update app.py

--- app.py
+++ app.py
@@ -1,11 +1,8 @@
 import streamlit as st
 from PIL import Image
-import matplotlib.pyplot as plt
-import pandas as pd
 import cv2
 import numpy as np
-from matplotlib.patches import Rectangle
-from keras.models import load_model
+from tensorflow.keras.models import load_model
 
 def load_model():
 
@@ -50,7 +47,6 @@ def predict(name):
         Prediction = NeuralNetwork.predict(SamplePhotoXTrain)
         FloatNumber = (1.0 - Prediction[0][0]) * 100
         ANS = str("%.2f" % FloatNumber)
-        # ANS = str("%.2f" % (1.0 - Prediction[0][0], 3) * 100)   
         if FloatNumber > 60:
           st.markdown("<h4 style='text-align: center; color: white;'>Обнаружены признаки пневмонии.</h4>", unsafe_allow_html=True)
           st.markdown(f"<h5 style='text-align: center; color: white;'>Вероятность наличия составляет {ANS}%</h5>", unsafe_allow_html=True)
